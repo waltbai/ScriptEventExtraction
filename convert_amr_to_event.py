79a3916b774e46ee1f17f3ac5a039ab53110e759
waltbai <bailong18b@ict.ac.cn>
2021-11-20 00:15:28 +0800

    Convert amr to event.

--- convert_amr_to_event.py
+++ convert_amr_to_event.py
@@ -1,1 +1,8 @@
 """Convert amr graph to event structure."""
+from amrgraph import AMRGraph
+
+
+def convert_amr_to_event(graph: AMRGraph):
+    """Convert amr graph to event structure."""
+    for e in graph.get_event_nodes():
+        pass
